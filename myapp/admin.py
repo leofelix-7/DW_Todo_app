39873505a9084dac8566d1e7bc3954988bf57185
Leo <richard7stz@gmail.com>
2025-08-03 11:07:45 +0630

    BIGGEST COMMIT

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import TodoModel, Tag # added aaaa
+from .models import TodoModel, Tag
 # Register your models here.
 
 admin.site.register(TodoModel)
-admin.site.register(Tag) # add by me
+admin.site.register(Tag)
--- myapp/urls.py
+++ myapp/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('list/', views.TodoList, name="TodoList"),
-    path('toggle-completed/<uuid:todo_id>/', views.toggle_completed, name="toggle_completed"),
+    path('', views.TodoList, name="TodoList"),
 ]
